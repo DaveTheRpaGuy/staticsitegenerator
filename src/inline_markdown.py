98ab415376e3bc5828161708d7ea77ecd60434c0
davetherpaguy <dave@davetherpaguy.com>
2025-02-25 10:35:58 -0500

    Cepacol

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,3 +1,5 @@
+import re
+
 from textnode import TextNode, TextType
         
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -16,3 +18,42 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             else:
                 new_nodes.append(TextNode(node.text, TextType.TEXT))
     return new_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.IMAGE.value:
+            new_nodes.append(node)
+        else:
+            split_texts = node.text.split(delimiter)
+            if len(split_texts) == 3:
+                new_nodes.append(TextNode(split_texts[0], TextType.TEXT))
+                new_nodes.append(TextNode(split_texts[1], TextType.IMAGE))
+                new_nodes.append(TextNode(split_texts[2], TextType.TEXT))
+            elif len(split_texts) == 2:
+                raise Exception("invalid markdown syntax")
+            else:
+                new_nodes.append(TextNode(node.text, TextType.TEXT))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.LINK.value:
+            new_nodes.append(node)
+        else:
+            something = ""
+    return new_nodes
+
+def extract_markdown_images(text):
+    regex = r"!\[([\w\s]+?)\]\((\w+:\/\/.*?)\)"
+    matches = re.findall(regex, text)
+    #print("print image matches...")
+    #print(matches)
+    return matches
+
+def extract_markdown_links(text):
+    regex = r"[^!]\[([\w\s]+?)\]\((\w+:\/\/.*?)\)"
+    #print("print link matches...")
+    matches = re.findall(regex, text)
+    return matches
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -13,4 +13,27 @@ class TestInlineMarkdown(unittest.TestCase):
             TextNode(" word", TextType.TEXT),
         ]
         self.assertEqual(expected, new_nodes)
-        
+    
+    def test_images_basic(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        actual = extract_markdown_images(text)
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(expected, actual)
+
+    def test_links_basic(self):
+        text = "This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif) and [obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        actual = extract_markdown_links(text)
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(expected, actual)
+
+    def test_images_partial(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and [obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        actual = extract_markdown_images(text)
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif")]
+        self.assertEqual(expected, actual)
+
+    def test_links_partial(self):
+        text = "This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        actual = extract_markdown_links(text)
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif")]
+        self.assertEqual(expected, actual)
